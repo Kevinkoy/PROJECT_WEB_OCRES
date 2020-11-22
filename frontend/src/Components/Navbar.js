83e24c888f47ba9ab9f8775c55249fafd5205b08
Kevin <koy-kevin@hotmail.fr>
2020-11-22 19:35:44 +0100

    Navbar

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    /// no Constructor
+
+
+    /// Methode Render
+    render() {
+        return (
+            <div>
+                <h1> Here NavBar </h1>
+            </div>
+        );
+    }
+
+}
+
+export default Navbar;
