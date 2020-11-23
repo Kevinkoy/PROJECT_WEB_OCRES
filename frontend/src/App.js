0fe27778d818c10a72b3f75e0327d9d78405de50
Kevin <koy-kevin@hotmail.fr>
2020-11-23 10:10:20 +0100

    Widget1/Widget2
    calendar/youtube

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import './App.css';
+/// Components
 import Navbar from './Components/Navbar';
 import Widget1 from './Components/Widget1';
+import Widget2 from './Components/Widget2'
+
 /// Grid = Gérer le display
-import { Container, Row, Col } from 'react-grid-system'; 
+import { Container, Row, Col } from 'react-grid-system';
 
 class App extends React.Component {
 
   render() {
-return (
-    <div>
-    <Container>
-  <Row>
-    <Col sm={4}>
-      <Widget1></Widget1>
-    </Col>
-    <Col sm={4}>
-      <Widget1></Widget1>
-    </Col>
-    <Col sm={4}>
-      <Widget1></Widget1>
-    </Col>
-  </Row>
-</Container>
-    </div>  
-  );
+    return (
+
+      <div>
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <Widget1 />
+            </Col>
+            <Col sm={4}>
+            
+        </Col>
+            <Col sm={4}>
+            <Widget2/>
+        </Col>
+          </Row>
+        </Container>
+
+      </div>
+
+    )
   }
 
 }
--- frontend/src/Components/Widget1.js
+++ frontend/src/Components/Widget1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 class Widget1 extends React.Component {
     /// no Constructor
