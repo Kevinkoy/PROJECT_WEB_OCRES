a9c99b4321d04aec147dd8eb0602ab8c7dff0300
Kevin <koy-kevin@hotmail.fr>
2020-11-26 19:49:00 +0100

    Routes / Admin
    npm i react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,58 @@
 import React from 'react';
 import './App.css';
+/// Grid = Gérer le display
 import { Container, Row, Col, setConfiguration } from 'react-grid-system';
 
-///Widgets
+/// Components
+import Navbar from './Components/Navbar';
 import Widget1 from './Components/Widget1';
-import Widget2 from './Components/Widget2';
+//import Widget2 from './Components/Widget2'
 import Widget3 from './Components/Widget3';
 import Widget4 from './Components/Widget4';
 import Widget5 from './Components/Widget5';
-import Widget6 from './Components/Widget6';
-import Widget7 from './Components/Widget7';
-import Widget9 from './Components/Widget9';
+//import Widget7 from './Components/Widget7';
+import Button from '@storybook/react/dist/demo/Button';
+import Admin from './Admin';
+//import { action } from '@storybook/addon-actions';
 
+/// Routes
 
+import { Link } from 'react-router-dom';
 
 class App extends React.Component {
- 
+
   render() {
 
     return (
-
       <div>
-        {/* Commentaire */}
-<<<<<<< HEAD
-        <Container fluid >
-            <Row justify="around" debug>
-              <Col xs={6} debug style={{marginTop:'200px', marginLeft:"30px", backgroundColor:"rgba(255, 122, 193, 0.19)"}}> <Widget2/> </Col>
-              <Col xs={3} debug style={{marginTop:'200px', backgroundColor: "rgba(0,0,0,0)"}}> <Widget1/> </Col>  
-            </Row> 
-            <br />
-            <br />
-            <Row justify="around" debug>
-              <Col xs={2} debug style={{marginLeft:"30px", backgroundColor:"rgba(0,0,0,0)"}}> <Widget5/> </Col>
-              <Col xs={2} debug style={{marginLeft:"30px", background:"rgba(0,0,0,0)"}}> <Widget3/> </Col>
-              <Col xs={2} debug style={{marginLeft:"30px", backgroundColor:"rgba(0,0,0,0)", marginTop:"20px"}}> <Widget4/> </Col>
-            </Row>
-=======
+        <Navbar/> 
+        {/* CONTAINERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR */}
         <Container>
-          <Row>
-            <Col sm={6}>
-              <Widget1 />
-            </Col>
-            <Col sm={6}>
-              <Widget2/>
-            </Col>
-            <Col sm={6}>
-              <Widget3/>
-            </Col>
-          </Row>
-          <Row>
+          <Row>    {/* 1er LIGNE */}
             <Col sm={4}>
-              <Widget4/>
+              <Widget1 /> {/* Widget */}
             </Col>
-            <Col sm={6}>
-              <Widget7/>
+            <Col sm={4}>
+              <Widget3 /> {/* Widget */}
             </Col>
-            <Col sm={6}>
-              <Widget9/>
+            <Col sm={4}>
 
             </Col>
           </Row>
->>>>>>> master
+          <Row> {/* 2EME LIGNE */}
+            <Col sm={4}>
+              <Widget4 /> {/* Widget */}
+            </Col>
+            <Col sm={4}>
+              <Widget5 /> {/* Widget */}
+            </Col>
+            <Col sm={4}>
+              {/* Vide */}
+            </Col>
+          </Row>
         </Container>
 
       </div>
-
     )
   }
 
