1cd282dc7ba24fab5b3ee40a8e3edc81587a981b
Kevin <koy-kevin@hotmail.fr>
2020-12-11 13:25:07 +0100

    ?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 /// Grid = Gérer le display
-import { Container, Row, Col } from 'react-grid-system';
+//import { Container, Row, Col } from 'react-grid-system';
 
 /// Components
 import Header from './Components/Header';
@@ -48,7 +48,7 @@ class App extends React.Component {
             {/* CONTAINER LE RESPONSIVE SE GERE DANS LES DIV AVEC col-xs | col-md | col-xl */}
             <div className="container">
 
-              {/* FIRST ROW 1 */} 
+              {/* FIRST ROW 1 */}
               <div className="row justify-content-center">
 
                 <div className="col-sm-6 col-md-8 col-lg-8">
@@ -58,7 +58,7 @@ class App extends React.Component {
                 </div>
 
                 <div className="col-sm-6 col-md-8 col-lg-4">
-                  <div className="card clean-card text-center" style={{background:"transparent", border:"transparent"}}>
+                  <div className="card clean-card text-center" style={{ background: "transparent", border: "transparent" }}>
                     <Widget1 /> {/* CALENDAR */}
                   </div>
                 </div>
@@ -73,20 +73,20 @@ class App extends React.Component {
 
                 <div className="col-sm-6 col-md-8 col-lg-6">
                   <div className="card clean-card text-center"
-                  style={{background:"radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)"}}
+                    style={{ background: "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)" }}
                   >
                     <Widget9 /> {/* GRAPH2 */}
                   </div>
                 </div>
 
                 <div className="col-sm-6 col-md-8 col-lg-3">
-                  <div className="card clean-card text-center"  style={{marginTop:"25px", background:"transparent", border:"transparent"}}>
-                    <Widget11/> {/* NOTIFICATIONS */}
+                  <div className="card clean-card text-center" style={{ marginTop: "25px", background: "transparent", border: "transparent" }}>
+                    <Widget11 /> {/* NOTIFICATIONS */}
                   </div>
                 </div>
 
                 <div className="col-sm-6 col-md-8 col-lg-3">
-                  <div className="card clean-card text-center" style={{background:"transparent", border:"transparent"}}>
+                  <div className="card clean-card text-center" style={{ background: "transparent", border: "transparent" }}>
                     <Widget8 /> {/* CLOCK */}
                   </div>
                 </div>
@@ -102,7 +102,7 @@ class App extends React.Component {
 
 
                 <div className="col-sm-6 col-md-8 col-lg-3">
-                  <div className="card clean-card center" style={{background:"transparent", border:"transparent"}}>
+                  <div className="card clean-card center" style={{ background: "transparent", border: "transparent" }}>
                     <Widget4 /> {/* DEEZER */}
                   </div>
                 </div>
@@ -110,13 +110,13 @@ class App extends React.Component {
 
 
                 <div className="col-sm-6 col-md-8 col-lg-4">
-                  <div className="card clean-card text-center" style={{background:"linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"}}>
+                  <div className="card clean-card text-center" style={{ background: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)" }}>
                     <Widget6 /> {/* WEATHER */}
                   </div>
                 </div>
- 
+
                 <div className="col-sm-6 col-md-8 col-lg-4">
-                  <div className="card clean-card text-center" style={{background:"transparent", border:"transparent"}}>
+                  <div className="card clean-card text-center" style={{ background: "transparent", border: "transparent" }}>
                     <Widget5 /> {/* POST-IT */}
                   </div>
                 </div>
