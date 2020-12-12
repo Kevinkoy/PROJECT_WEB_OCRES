88a2fc5adae1af074a8f46649ea33b5e93b5ae0f
Kevin <koy-kevin@hotmail.fr>
2020-12-13 00:45:38 +0100

    Add Update Delete

--- frontend/src/Components/Stickynotes.js
+++ frontend/src/Components/Stickynotes.js
@@ -10,10 +10,11 @@ class StickyNotes extends Component {
 
         /// this.state.attributs
         this.state = {
+            id:'',
             title: '',
             notes: '',
             /// MAP POUR STOCKER 
-            allNote: [
+            Collection: [
                 {
                     title: '',
                     notes: ''
@@ -24,13 +25,17 @@ class StickyNotes extends Component {
 
         /// Binding 
         this.componentDidMount = this.componentDidMount.bind(this)
+        this.onChangeId =this.onChangeId.bind(this)
         this.onChangeTitle = this.onChangeTitle.bind(this)
         this.onChangeNotes = this.onChangeNotes.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+        this.onSubmitAdd = this.onSubmitAdd.bind(this)
+        this.onSubmitUpdate = this.onSubmitUpdate.bind(this)
+        this.onSubmitDelete = this.onSubmitDelete.bind(this)
 
     }
 
     /// Methode componentDidMount
+
     async componentDidMount() {
         try {
             await axios.get("http://localhost:5000/StickyNotes")
@@ -48,9 +53,9 @@ class StickyNotes extends Component {
                       }
                       */
                     this.setState({
-                        allNote: data
+                        Collection: data
                     })
-                    console.log("Collection stickynote:", this.state.allNote)
+                    console.log("Collection stickynote:", this.state.Collection)
                 })
                 .catch(err => {
                     console.log(err)
@@ -61,6 +66,12 @@ class StickyNotes extends Component {
     }
 
     /// Methodes onChange & onSave
+    onChangeId(e) {
+        this.setState({ 
+            id: e.target.value
+        })
+    }
+
     onChangeTitle(e) {
         this.setState({ // Update the notes state
             title: e.target.value
@@ -72,8 +83,10 @@ class StickyNotes extends Component {
         })
     }
 
-    /// AJOUTER
-    onSubmit(e) {
+
+
+    /// ADD
+    onSubmitAdd(e) {
         e.preventDefault();
         const stickynote = {
             title: this.state.title,
@@ -86,8 +99,40 @@ class StickyNotes extends Component {
             .then(res => {
                 console.log(res.data)
             });
-        window.location = '/';
+         window.location = '/stickynotes';
     }
+
+        /// Update
+        onSubmitUpdate(e) {
+            e.preventDefault();
+            const stickynote = {
+                title: this.state.title,
+                notes: this.state.notes,
+            }
+           
+            console.log(stickynote);
+            const url = 'http://localhost:5000/stickynotes/update/' + this.state.id;
+            axios.post(url, stickynote)
+                .then(res => {
+                    console.log(res.data)
+                });
+             window.location = '/stickynotes';
+        }
+
+         /// Update
+         onSubmitDelete(e) {     
+            const stickynote = {
+                title: this.state.title,
+                notes: this.state.notes,
+            }
+            console.log(stickynote);
+            const url = 'http://localhost:5000/stickynotes/' + this.state.id;
+            axios.delete(url)
+                .then(res => {
+                    console.log(res.data)
+                });
+             window.location = '/stickynotes';
+        }
 
     /// SUPPRIMER
     clickElem(e) {
@@ -103,6 +148,8 @@ class StickyNotes extends Component {
 
     /// METHODE RENDER !!!
     render() {
+
+
         return (
 
             <div>
@@ -121,7 +168,7 @@ class StickyNotes extends Component {
                         <div className="col-sm-6 col-md-6 col-lg-4">
                             <h3>Create Sticky Notes</h3>
 
-                            <form onSubmit={this.onSubmit}>
+                            <form onSubmit={this.onSubmitAdd}>
                                 <div className="form-group">
                                     <label> Title </label>
                                     <input type="text"
@@ -153,7 +200,19 @@ class StickyNotes extends Component {
                         <div className="col-sm-6 col-md-6 col-lg-4">
                             <h3> Update (by _id) </h3>
 
-                            <form onSubmit={this.onSubmit}>
+                            <form onSubmit={this.onSubmitUpdate}>
+
+                                <div className="form-group">
+                                    <label> _id </label>
+                                    <input type="text"
+                                        required
+                                        className="form-control"
+                                        value={this.state.id}
+                                        onChange={this.onChangeId}
+                                        
+                                    />
+                                </div>
+
                                 <div className="form-group">
                                     <label> Title </label>
                                     <input type="text"
@@ -175,7 +234,7 @@ class StickyNotes extends Component {
                                 </div>
 
                                 <div className="form-group">
-                                    <input type="submit" value="Update StickyNotes" className="btn btn-success btn-block" />
+                                    <input type="submit" value="Update StickyNotes" className="btn btn-warning btn-block" />
                                 </div>
                             </form>
                         </div>
@@ -184,29 +243,21 @@ class StickyNotes extends Component {
                         <div className="col-sm-6 col-md-6 col-lg-4">
                             <h3>Delete (by _id) </h3>
 
-                            <form onSubmit={this.onSubmit}>
+                            <form onSubmit={this.onSubmitDelete}>
                                 <div className="form-group">
-                                    <label> Title </label>
+                                    <label> _id </label>
                                     <input type="text"
                                         required
                                         className="form-control"
-                                        value={this.state.title}
-                                        onChange={this.onChangeTitle}
+                                        value={this.state.id}
+                                        onChange={this.onChangeId}
                                     />
                                 </div>
 
-                                <div className="form-group">
-                                    <label> Notes </label>
-                                    <input type="text"
-                                        required
-                                        className="form-control"
-                                        value={this.state.notes}
-                                        onChange={this.onChangeNotes}
-                                    />
-                                </div>
+                               
 
                                 <div className="form-group">
-                                    <input type="submit" value="Delete StickyNotes" className="btn btn-success btn-block" />
+                                    <input type="submit" value="Delete StickyNotes" className="btn btn-danger btn-block" />
                                 </div>
                             </form>
                         </div>
@@ -237,22 +288,25 @@ class StickyNotes extends Component {
                             <tbody>
                                 {/* BOUCLE FOR pour créer nombre de ligne en fonction du nb de row dans la collection */}
 
+
+
+
                                 <tr>
-                                    <td className="text-center">{this.state.allNote[0]._id}</td>
-                                    <td className="text-center">{this.state.allNote[0].title}</td>
-                                    <td className="text-center">{this.state.allNote[0].notes}</td>
-                                    <td className="text-center">{this.state.allNote[0].createdAt}</td>
-                                    <td className="text-center">{this.state.allNote[0].updatedAt}</td>
-                                    <td className="text-center">{this.state.allNote[0].__v}</td>
+                                    <td className="text-center">{this.state.Collection[0]._id}</td>
+                                    <td className="text-center">{this.state.Collection[0].title}</td>
+                                    <td className="text-center">{this.state.Collection[0].notes}</td>
+                                    <td className="text-center">{this.state.Collection[0].createdAt}</td>
+                                    <td className="text-center">{this.state.Collection[0].updatedAt}</td>
+                                    <td className="text-center">{this.state.Collection[0].__v}</td>
                                 </tr>
 
                                 <tr>
-                                    <td className="text-center">{this.state.allNote[0]._id}</td>
-                                    <td className="text-center">{this.state.allNote[0].title}</td>
-                                    <td className="text-center">{this.state.allNote[0].notes}</td>
-                                    <td className="text-center">{this.state.allNote[0].createdAt}</td>
-                                    <td className="text-center">{this.state.allNote[0].updatedAt}</td>
-                                    <td className="text-center">{this.state.allNote[0].__v}</td>
+                                    <td className="text-center">{this.state.Collection[0]._id}</td>
+                                    <td className="text-center">{this.state.Collection[0].title}</td>
+                                    <td className="text-center">{this.state.Collection[0].notes}</td>
+                                    <td className="text-center">{this.state.Collection[0].createdAt}</td>
+                                    <td className="text-center">{this.state.Collection[0].updatedAt}</td>
+                                    <td className="text-center">{this.state.Collection[0].__v}</td>
                                 </tr>
 
 
